bf8a3b3d5602fd400b53747244cc80476a2d8678
Jokubas <jokbern@gmail.com>
2026-01-31 01:25:37 +0200

    player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float MoveSpeed;
+    float verticalMov;
+    float horizontalMov;
+    private Rigidbody RB;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        RB = GetComponent < Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        verticalMov = Input.GetAxisRaw("Vertical");
+        horizontalMov = Input.GetAxisRaw("Horizontal");
+        Debug.Log(verticalMov);
+        RB.AddForce(horizontalMov * MoveSpeed, 0, verticalMov * MoveSpeed);
+
+        RB.transform.rotation = Quaternion.LookRotation(RB.linearVelocity, transform.up);
     }
 }
