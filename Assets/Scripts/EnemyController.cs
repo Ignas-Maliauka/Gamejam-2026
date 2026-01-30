8590e1a40c66d865c8873f29c980f302d6766852
Redoactive <ignasteris456@gmail.com>
2026-01-31 01:33:36 +0200

    Added enemy simple ai

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -8,11 +9,18 @@ public class EnemyController : MonoBehaviour
         Idle,
         Walk
     }
+    public float arcAngle;
+    public float turnBackAngle;
     public float idleTimer;
     private Vector3 walkTarget;
     public EnemyStates currentState = EnemyStates.Idle;
+    public float pointDistanceMultiplier;
 
     public NavMeshAgent agent;
+    private void Start()
+    {
+        transform.rotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
+    }
     void Update()
     {
         if(currentState == EnemyStates.Idle)
@@ -26,7 +34,7 @@ public class EnemyController : MonoBehaviour
         }
         else if(currentState == EnemyStates.Walk)
         {
-            if(Vector3.Distance(transform.position, walkTarget) < 1f)
+            if(Vector3.Distance(transform.position, walkTarget) < 3f)
             {
                 currentState = EnemyStates.Idle;
                 idleTimer = Random.Range(0, 0.2f);
@@ -36,18 +44,56 @@ public class EnemyController : MonoBehaviour
     
     private void setDestination()
     {
-        for(int i = 0; i < 1000; i++){
-            walkTarget = new Vector3(Random.Range(-5f, 5f), 0, Random.Range(-5f, 5f));
-            if (agent.SetDestination(walkTarget))
+        for(int i = 0; i < 10; i++){
+            walkTarget = GetPointInArc();
+            walkTarget.y = 0;
+            NavMeshPath path = new NavMeshPath();
+            if (agent.CalculatePath(walkTarget, path) && path.status == NavMeshPathStatus.PathComplete)
             {
-                break;
+                agent.SetDestination(walkTarget);
+
+                return;
+            }
+
+        }
+
+        for (int i = 0; i < 100; i++)
+        {
+            walkTarget = GetPointBehind();
+            walkTarget.y = 0;
+            NavMeshPath path = new NavMeshPath();
+            if (agent.CalculatePath(walkTarget, path) && path.status == NavMeshPathStatus.PathComplete)
+            {
+                agent.SetDestination(walkTarget);
+
+                return;
             }
         }
-        
+        Debug.Log("Failed to find path for enemy");
     }
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.greenYellow;
         Gizmos.DrawSphere(walkTarget, 1f);
     }
+    private Vector3 GetPointInArc()
+    {
+        float angle = Random.Range(-arcAngle, arcAngle);
+        Quaternion rotation = Quaternion.AngleAxis(angle, transform.up);
+        Vector3 direction = rotation * transform.forward;
+        return transform.position + (direction * pointDistanceMultiplier);
+    }
+    private Vector3 GetPointBehind()
+    {
+        Vector3 directionToCenter = (Vector3.zero - transform.position).normalized;
+        float angle = Mathf.Atan2(directionToCenter.x, directionToCenter.z) * Mathf.Rad2Deg;
+        angle += Random.Range(-turnBackAngle, turnBackAngle);
+        Quaternion rotation = Quaternion.AngleAxis(angle, transform.up);
+
+        Vector3 direction = rotation * Vector3.forward;
+        return transform.position + (direction * pointDistanceMultiplier);
+    }
 }
+
+
+  
