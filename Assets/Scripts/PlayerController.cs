bdb4392b531d8ed1cb5d218a0ebe826dd2e3a7ff
Redoactive <ignasteris456@gmail.com>
2026-01-31 02:38:26 +0200

    Added game manager logic for game over

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,9 +6,11 @@ public class PlayerController : MonoBehaviour
     float verticalMov;
     float horizontalMov;
     private Rigidbody RB;
+    private GameManager gameManager;
 
     void Start()
     {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
         RB = GetComponent < Rigidbody>();
     }
     void FixedUpdate()
@@ -19,4 +21,12 @@ public class PlayerController : MonoBehaviour
 
         RB.transform.rotation = Quaternion.LookRotation(RB.linearVelocity, transform.up);
     }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            gameManager.GameOver();
+        }
+    }
+
 }
